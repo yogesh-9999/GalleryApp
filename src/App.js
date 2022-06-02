f0bb25f93a2fc6bc6cfea6f2a137b927c97835fe
YOGESH CHAND UPADHYAY <84492118+yogesh-9999@users.noreply.github.com>
2022-06-03 02:32:44 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
-
+import react, { useEffect, useState } from "react";
+import axios from "axios";
+// import Images from "./components/Images";
+import "./App.css";
+import { saveAs } from 'file-saver'
+import {BsFillArrowDownSquareFill} from 'react-icons/bs'
+// import { ReactDOM } from "react";
 function App() {
+  const [photo, setPhoto] = useState("Cat");
+  const [images, setImages] = useState([]);
+  const [clientId, setClientId] = useState(
+    "SUSMi5XfkX8WP-82y1I-oy2_GX9h-V7DMteJgfA128g&per_page=30&page=2"
+  );
+  const downloadImage = (image_url,name) => {
+    saveAs(image_url, name) // Put your image url here.
+  }
+  
+  const handlesubmit = async (e) => {
+    e.preventDefault();
+    const url = `https://api.unsplash.com/search/photos?client_id=${clientId}&query=${photo}&per_page=50`;
+    // const url="https://api.unsplash.com/photos/?client_id=SUSMi5XfkX8WP-82y1I-oy2_GX9h-V7DMteJgfA128g&per_page=30&page=2"
+    const res = await axios.get(url);
+    console.log(res);
+    setImages(res.data.results);
+
+    
+  };
+  useEffect(() => {
+    handlesubmit();
+  
+  }, {photo})
+  
+
+  // useEffect(async()=>{
+  //   const url="https://api.unsplash.com/photos/?client_id=SUSMi5XfkX8WP-82y1I-oy2_GX9h-V7DMteJgfA128g&per_page=30&page=2";
+  //   const res = await axios.get(url);
+  //   console.log(res);
+  //   setImages(res.data.results);
+
+  // },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <br />
+      <form>
+        <input
+          value={photo}
+          onChange={(e) => {
+            setPhoto(e.target.value);
+          }}
+          type="text"
+          name="photo"
+          placeholder="Search"
+        />
+
+        <button onClick={handlesubmit}>
+          Search
+        </button>
+      </form>
+      {/* <button className="btn btn-primary btn-md" onClick={fetchapi}>
+        Fetch
+      </button> */}
+      <br />
+      <div className="photos">
+        {images != undefined &&
+          images.map((image) => {
+            return (
+              <div className="image-container">
+                  <img className="m-1" key={image.id} src={image.urls.small} />
+                  <BsFillArrowDownSquareFill onClick={()=>{downloadImage(image.urls.full,image.id)}} className='downloadBtn'/>
+                
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
